43cf6940e910a81e0033ff0819cae7ff0e877e6c
Samker <samkergachu@gmail.com>
2022-05-13 13:25:10 +0300

    updated version of sort algo

--- chapter1/sort.c
+++ chapter1/sort.c
@@ -4,98 +4,81 @@
 
 #define MaxNumbers 10
 
-//implement selection sort
+//program to implement the selection Sort algorithm.
 
-//get smallest value.
 
-int getSmallest(int list[], int lo, int hi) {
-	//return locatin of smallest value.
+void selectionSort(int Array[], int lowest, int highest) {
+	//function prototype
+	int getSmallest(int Array[], int low, int high);
+	void swap(int Array[], int smallest, int small);
 	
-	int smallest = lo; 
-
-	for(int i = lo + 1; i < hi; i++) 
-		if(list[i] < list[smallest]) smallest = i; 
-
-	return smallest;
-}
-
-
-//swap function
-void swap(int list[], int i, int j) {
-
-	//swap elements list[i] and list[j]
-	int hold = list[i]; 
-	list[i] = list[j]; 
-	list[j] = hold; 
-
-}
-
-
-
-void selectionSort(int list[], int lo, int hi) {
-
-	//TODO
-	int getSmallest(int[], int, int); 
-	void swap(int[], int, int);
-
-	for(int h = lo; h < hi; h++) {
-
-		int s = getSmallest(list, h, hi); 
-		swap(list, h, s); 
+	for(int h = lowest; h < highest; h++) {
+		int smallest = getSmallest(Array, h, highest); 
+		swap(Array, smallest, h); 
 	}
 
 }
 
-//insertion sort
 
-void insertionSort(int list[], int n) {
-	//TODO
-	
-	for(int h = 1; h < n; h++) {
-		int key = list[h]; 
-		int k = h-1; //start comparing with previous item;
+//find smallest no.
 
-		while( k >= 0 && key < list[k]) {
+int getSmallest(int Array[], int low, int high) {
+	void swap(int Array[], int smallest, int small); 
 
-			list[k+1] = list[k]; 
-			--k; 
+	int lowest = low; 
+
+	for(int k = low+1; k <= high; k++){
+		if (Array[k] < Array[lowest]) {
+			lowest = k; 
 		}
+	}
 
-		list[k+1] = key; 
-	} //end for
+	return lowest;
+}
 
-} //end insertion sort
+void swap(int Array[], int smallest, int small) {
+
+	int holder = Array[small];
+	Array[small] = Array[smallest];
+	Array[smallest] = holder; 
+}
+
+//print array
 
 
+void print(int Array[], int lowest, int highest) {
 
-int main() {
+	for(int i = lowest; i < highest; i++){
+		printf("%d ", Array[i]);
+	}
 
-	int num[MaxNumbers]; 
-	printf("Type up to %d numbers followed by 0\n", MaxNumbers); 
+	printf("\n");
+}
 
-	int n = 0, v; 
-	scanf("%d", &v);  //input
 
-	while( v != 0 && n < MaxNumbers) {
+int main(){
+	//function prototype
+	void selectionSort(int Array[], int lowest, int highest); 
+	void print(int Array[], int lowest, int highest);
 
+	int num[MaxNumbers];
+	int n = 0, v;
+
+	scanf("%i", &v); 
+
+	while (v != 0 && n < MaxNumbers) {
 		num[n++] = v; 
 		scanf("%d", &v); 
 	}
 
 	if (v != 0) {
-		printf("More than %d numbers entered\n", MaxNumbers); 
-		printf("First %d used\n", MaxNumbers); 
+		printf("More than %d entered\n", MaxNumbers);
+		printf("We'll use the first %d numbers\n", MaxNumbers); 
 	}
-	/*
+
 	selectionSort(num, 0, n); 
-	printf("\nThe sorted numbers are\n"); 
-	*/
 
-	insertionSort(num, n); 
-	printf("\nThe sorted numbers are\n"); 
-	for(int h = 0; h < n; h++) printf("%d ", num[h]); 
+	print(num, 0, n); 
 
-	printf("\n"); 
-
-} //end of main
-
+	return 0; 
+}
